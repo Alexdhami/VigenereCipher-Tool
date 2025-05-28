c98e8fa03d713b4c4fb9651ef3b9e13ed1394e1a
Alexdhami <alexdhami864@gmail.com>
2025-05-28 11:29:20 +0545

    CLI version solid for terminal; importable, but use module version for best experience

--- vigenere_cipher_cli.py
+++ vigenere_cipher_cli.py
@@ -1,82 +1,70 @@
 class EncryptionDecryption:
     def __init__(self):
-        self.ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.SHIFTED = ''
-        for i in range(26):
-            self.SHIFTED += self.ALPHABET[i:] + self.ALPHABET[:i] + "\n"
-        self.SHIFTED = self.SHIFTED.split()
-        
-    def encryption_function(self,plain_text,key):
+        self.all_ascii_characters = ''.join([chr(i) for i in range(32, 127)])
+
+        self.all_ascii_characters_table = []
+        self.corresponding_ascii_indexes = {}
+        for i in range(len(self.all_ascii_characters)):
+            copy = self.all_ascii_characters[i:] + self.all_ascii_characters[:i]
+            self.all_ascii_characters_table.append(copy)
+            self.corresponding_ascii_indexes[self.all_ascii_characters[i]] = i
+
+    def encryption_function(self, text, key):
         new_key = ''
         index = 0
-        plain_text = plain_text.replace(" ",'')
-
-        while len(new_key) < len(plain_text):
+        while len(new_key) < len(text):
             new_key += key[index]
             index += 1
             if index >= len(key):
                 index = 0
+        encoded_word = ''
+        for i in range(len(text)):
+            if text[i] in self.all_ascii_characters and new_key[i] in self.all_ascii_characters:
+                key_index = self.corresponding_ascii_indexes[new_key[i]]
+                text_index = self.corresponding_ascii_indexes[text[i]]
+                encoded_word += self.all_ascii_characters_table[key_index][text_index]
+            else:
+                encoded_word += text[i]
+        return encoded_word
 
-        encrypted_word = ''
-        index_char = []
-        index_key = []
-
-        for char in plain_text:
-            index_char.append(ord(char) - 65)
-
-        for char in new_key:
-            index_key.append(ord(char) - 65)
-
-        for i in range(len(plain_text)):
-            encrypted_word += self.SHIFTED[index_char[i]][index_key[i]]
-
-        return encrypted_word
-
-    def decryption_function(self,enword,key):
+    def decryption_function(self, text, key):
         new_key = ''
-
         index = 0
-
-        while len(new_key) < len(enword):
+        while len(new_key) < len(text):
             new_key += key[index]
             index += 1
             if index >= len(key):
                 index = 0
+        decoded_word = ''
+        for i in range(len(text)):
+            if text[i] in self.all_ascii_characters and new_key[i] in self.all_ascii_characters:
+                key_index = self.corresponding_ascii_indexes[new_key[i]]
+                row = self.all_ascii_characters_table[key_index]
+                col_index = row.index(text[i])
+                decoded_word += self.all_ascii_characters[col_index]
+            else:
+                decoded_word += text[i]
+        return decoded_word
 
-        plain_text = ''
-
-        index_key = []
-
-
-        for char in new_key:
-            index_key.append(ord(char) - 65)
-
-        for i in range(len(enword)):
-                row = index_key[i]
-                col = self.SHIFTED[row].index(enword[i])
-                plain_text += chr(col + 65)                
-                
-
-        return plain_text
 
     def main(self):
         while True:
-            choice = input('Enter do you want to quit? (yes/no): ').lower().replace(' ','')
+            choice = input('Enter do you want to quit? (yes/no): ').strip().lower()
             if choice == 'yes':
                 break
             elif choice == 'no':
-                choose = input('"e" for encryption and "d" for decryption: ').lower().replace(' ','')
+                choose = input('"e" for encryption and "d" for decryption: ').strip().lower()
 
                 
                 if choose == 'e':
-                    plain_text = input('Enter messege you want to encrypt: ').upper().replace(' ','')
+                    plain_text = input('Enter messege you want to encrypt: ')
 
-                    key = input('Enter the key: ').upper().replace(' ','')
-                    print(f"Your encrypted word {self.encryption_function(plain_text,key)}.")
+                    key = input('Enter the key: ')
+                    print(f"Your encrypted messege is :{self.encryption_function(plain_text,key)}")
 
                 elif choose == 'd':
-                    encrypted_word = input('Enter Encrypted word: ').upper().replace(' ','')
-                    key = input('Enter the key: ').upper().replace(' ','')
+                    encrypted_word = input('Enter Encrypted word: ')
+                    key = input('Enter the key: ')
                     print(f'Your plain text messege is : {self.decryption_function(encrypted_word,key)}')
                 else:
                     print('Invalid option')
